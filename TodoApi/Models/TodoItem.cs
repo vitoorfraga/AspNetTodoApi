a983d7212d2245227d9eb06253d6d3792c8efdd9
Vitor Fraga <vitorfragaps@gmail.com>
2024-08-31 23:42:08 -0300

    Add project files.

---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public bool? IsComplete { get; set; } = false;
+        public string? Secret {  get; set; }
+    }
+}
